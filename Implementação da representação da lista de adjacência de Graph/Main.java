7b46d5e4afe0cea28d859e3206987442eba3629f
Valmir Junior <61609240+Valmir-unicap@users.noreply.github.com>
2023-03-25 21:49:19 -0300

    Create Main.java

---
+++ Implementação da representação da lista de adjacência de Graph/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+  int V = 5;
+  Graph g = new Graph(V);
+  g.addEdge(0, 1);
+  g.addEdge(0, 3);
+  g.addEdge(1, 2);
+  g.addEdge(1, 3);
+  g.addEdge(2, 4);
+  g.addEdge(2, 3);
+  g.addEdge(3, 4);
+  g.print();
+  }
+}
